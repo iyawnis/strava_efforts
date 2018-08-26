eb28d9949fdea1a5527c2966b19f9d436e39659b
latusaki <latusaki@users.noreploy.github.com>
2018-08-26 21:47:50 +0000

    Add scheduled strava hit

--- clock.py
+++ clock.py
@@ -1,10 +1,18 @@
+from datetime import datetime
 from apscheduler.schedulers.blocking import BlockingScheduler
+from strava import get_efforts_for_segment
+from store import set_segment_date_count
+
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=3)
+@sched.scheduled_job('interval', hours=1)
 def timed_job():
+    segment_id = '7550717'
     print('This job is run every three minutes.')
+    effort_count = get_efforts_for_segment(seg_id)
+    set_segment_date_count(seg_id, datetime.today(), effort_count())
+
 
 @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
 def scheduled_job():
--- store.py
+++ store.py
@@ -1,8 +1,18 @@
 import os
-import redis
+import redis as _redis
 
 REDIS_URL = os.environ.get('REDISTOGO_URL')
 
-client = redis.from_url(REDIS_URL)
+redis = _redis.from_url(REDIS_URL)
 
+def set_segment_date_count(segment, date, count):
+    hkey = f'segment.{segment}'
+    key = date.strftime('%Y%m%d')
+    return redis.hset(hkey, key, count)
+
+def get_stored_segments():
+    res = redis.scan(0, 'segment.*')
+    if len(res) == 2:
+        return res[1]
+    return None
 
