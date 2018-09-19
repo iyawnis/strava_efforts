40ba5891a5a27a388fd281f97fa6aab26ad979b3
latusaki <latusaki@users.noreploy.github.com>
2018-09-19 20:42:46 +0000

    Fix scheduler to avoid tz errors

--- scheduler.py
+++ scheduler.py
@@ -7,13 +7,13 @@ logger = logging.getLogger(__name__)
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('cron', minute=0)
+@sched.scheduled_job('cron', hour=21)
 def day_job():
     logger.info("Storing today's counts")
     store_count_for_timeframe('today')
 
 
-@sched.scheduled_job('cron', hour=23, minute=50)
+@sched.scheduled_job('cron', hour=21)
 def month_job():
     logger.info("Storing month's counts")
     store_count_for_timeframe('month')
